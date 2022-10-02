106bb1de2af562673cce90f478ead6bdbc872242
xela <xelaxela13@gmail.com>
2022-10-02 14:37:17 +0200

    order get_total_amount with discount

--- shop/urls.py
+++ shop/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,3 +23,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(items_urlpatterns))
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
