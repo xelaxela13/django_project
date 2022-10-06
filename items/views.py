7e56e6d2701e3cbccc75d913e570bcdfc87cd2cc
xela <xelaxela13@gmail.com>
2022-10-06 11:34:09 +0200

    django forms

--- items/views.py
+++ items/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
+from items.model_forms import ItemModelForm
+from items.models import Item
+
 
 def items(request, *args, **kwargs):
-    return render(request, 'items/index.html')
+    if request.method == 'POST':
+        form = ItemModelForm(request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ItemModelForm()
+    context = {
+        'items': Item.objects.all(),
+        'form': form
+    }
+    return render(request, 'items/index.html', context=context)
--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.utils.safestring import mark_safe
 
 from orders.models import Order, Discount
 
