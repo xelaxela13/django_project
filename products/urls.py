1533648bd8a6fd012e3bef0caae91557f4dfe032
xela <xelaxela13@gmail.com>
2022-10-16 18:16:00 +0200

    import csv

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from products.views import ProductsView, ProductDetail, export_csv, ExportPDF
+from products.views import ProductsView, ProductDetail, export_csv, \
+    ExportPDF, ImportCSV
 
 urlpatterns = [
     path('products/', ProductsView.as_view(), name='products'),
     path('products/csv/', export_csv, name='export_csv'),
+    path('products/import/csv/', ImportCSV.as_view(), name='import_csv'),
     path('products/pdf/', ExportPDF.as_view(), name='export_pdf'),
     path('products/<uuid:pk>/', ProductDetail.as_view(),
          name='product_detail'),
