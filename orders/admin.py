b07d3bcdb56df6ea4e9ff388c096340ca0284e2d
xela <xelaxela13@gmail.com>
2022-10-22 22:41:52 +0200

    order table through

--- orders/admin.py
+++ orders/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 
-from orders.models import Order, Discount
+from orders.models import Order, Discount, OrderProductRelation
+
+
+class OrderProductRelationInline(admin.TabularInline):
+    model = OrderProductRelation
+    extra = 1
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ('discounted',)
     list_display = ('id', 'user', 'total_amount', 'discount', 'discounted')
-    filter_horizontal = ('products',)
+    inlines = (OrderProductRelationInline,)
 
     def discounted(self, obj=None):
         return obj.get_total_amount()
