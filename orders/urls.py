febfcdf4c0138845e6885339f988a29bf433b859
xela <xelaxela13@gmail.com>
2022-11-03 21:36:13 +0100

    favorites products

--- orders/urls.py
+++ orders/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path, re_path
 
 from orders.views import CartView, UpdateCartView, RecalculateCartView, \
@@ -6,10 +7,12 @@ from orders.views import CartView, UpdateCartView, RecalculateCartView, \
 urlpatterns = [
     path('cart/', CartView.as_view(), name='cart'),
     re_path(r'cart/(?P<action>add|remove|clear|pay)/',
-            UpdateCartView.as_view(),
+            login_required(UpdateCartView.as_view()),
             name='update_cart'),
-    path('recalculate/', RecalculateCartView.as_view(),
+    path('recalculate/',
+         login_required(RecalculateCartView.as_view()),
          name='recalculate_cart'),
-    path('apply-discount/', ApplyDiscountView.as_view(),
+    path('apply-discount/',
+         login_required(ApplyDiscountView.as_view()),
          name='apply_discount'),
 ]
--- products/urls.py
+++ products/urls.py
@@ -1,13 +1,21 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-from products.views import ProductsView, ProductDetail, export_csv, \
-    ExportPDF, ImportCSV
+from products.views import ProductsView, ProductDetailView, export_csv, \
+    ExportPDF, ImportCSV, FavoriteProductsView, FavoriteProductAddOrRemoveView
 
 urlpatterns = [
     path('products/', ProductsView.as_view(), name='products'),
     path('products/csv/', export_csv, name='export_csv'),
     path('products/import/csv/', ImportCSV.as_view(), name='import_csv'),
     path('products/pdf/', ExportPDF.as_view(), name='export_pdf'),
-    path('products/<uuid:pk>/', ProductDetail.as_view(),
+    path('products/<uuid:pk>/',
+         ProductDetailView.as_view(),
          name='product_detail'),
+    path('favorites/',
+         login_required(FavoriteProductsView.as_view()),
+         name='favorites'),
+    path('favorites/<uuid:pk>/add/',
+         login_required(FavoriteProductAddOrRemoveView.as_view()),
+         name='add_or_remove_favorite'),
 ]
