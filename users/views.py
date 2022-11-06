798297dbae9f4afe1a6c7c2e3a212ade3e4b9aee
xela <xelaxela13@gmail.com>
2022-11-06 10:57:48 +0100

    custom auth backend

--- users/views.py
+++ users/views.py
@@ -1,27 +1,20 @@
+from django.contrib import messages
 from django.contrib.auth import login
+from django.contrib.auth.views import LoginView as AuthLoginView
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, FormView
+from django.views.generic import FormView
 
-from users.forms import LoginForm
+from users.forms import CustomAuthenticationForm
 from users.model_forms import RegistrationForm
 
 
-# todo remove
-class LoginView(TemplateView):
-    template_name = 'registration/login.html'
+class LoginView(AuthLoginView):
+    form_class = CustomAuthenticationForm
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({'form': kwargs.get('form') or LoginForm})
-        return context
-
-    def post(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-        form = context['form']
-        form = form(request.POST)
-        if form.is_valid():
-            login(request, form.user)
-        return self.get(request, form=form, *args, **kwargs)
+    def form_valid(self, form):
+        messages.success(self.request,
+                         f'Welcome back {form.get_user().email}!')
+        return super().form_valid(form)
 
 
 class RegistrationView(FormView):
