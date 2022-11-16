de82f8b42ab2298b54c3ba7377933a61362cc612
xela <xelaxela13@gmail.com>
2022-11-16 20:25:51 +0100

    pytest mock coverage

--- conftest.py
+++ conftest.py
@@ -1,7 +1,11 @@
 import pytest
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 from faker import Faker
+from django.test.client import Client
 
 fake = Faker()
+User = get_user_model()
 
 
 @pytest.fixture(scope='session')
@@ -18,3 +22,35 @@ def enable_db_access_for_all_tests(db):
     yield
     del __builtins__['pp']
     # code after tests run
+
+
+@pytest.fixture(scope='function')
+def user(db):
+    user, _ = User.objects.get_or_create(
+        email='user@user.com',
+        first_name='John Smith',
+        phone='123456789',
+        is_phone_valid=True
+    )
+    user.set_password('123456789')
+    user.save()
+    yield user
+
+
+@pytest.fixture(scope='function')
+def login_user(db):
+    phone = '123456789'
+    password = '123456789'
+    user, _ = User.objects.get_or_create(
+        email='user@user.com',
+        first_name='John Smith',
+        phone=phone,
+        is_phone_valid=True
+    )
+    user.set_password(password)
+    user.save()
+    client = Client()
+    response = client.post(reverse('login'), data={'phone': phone,
+                                                   'password': password})
+    assert response.status_code == 302
+    yield client, user
---
+++ currencies/tests.py
@@ -0,0 +1,18 @@
+from currencies.models import CurrencyHistory
+from currencies.tasks import get_currencies
+
+
+def test_get_currencies_task(mocker, faker):
+    assert not CurrencyHistory.objects.exists()
+    get_currency = mocker.patch('currencies.cliens.cliens.privat_currency_client.get_currency')
+    get_currency.return_value = [
+        {'ccy': 'USD', 'buy': '2', 'sale': '3'},
+        {'ccy': 'EUR', 'buy': '4', 'sale': '5'},
+    ]
+    assert not get_currency.call_count
+
+    get_currencies()
+
+    assert get_currency.call_count
+    assert CurrencyHistory.objects.filter(currency='USD', sale='3')
+    assert CurrencyHistory.objects.filter(currency='EUR', sale='5')
