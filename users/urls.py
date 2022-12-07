f0a5fb41a8a68db58b2bb1b1e5af8746448bed6d
xela <xelaxela13@gmail.com>
2022-12-07 20:23:25 +0100

    registration change flow

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from users.views import RegistrationView, LoginView
+from users.views import RegistrationView, LoginView, RegistrationConfirmView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('registration/', RegistrationView.as_view(), name='registration'),
+    path('registration/<uidb64>/<token>/confirm/',
+         RegistrationConfirmView.as_view(),
+         name='registration_confirm'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,16 @@
 from django.contrib import messages
-from django.contrib.auth import login
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.views import LoginView as AuthLoginView
+from django.core.exceptions import ValidationError
 from django.urls import reverse_lazy
-from django.views.generic import FormView
+from django.utils.http import urlsafe_base64_decode
+from django.views.generic import FormView, RedirectView
 
 from users.forms import CustomAuthenticationForm
 from users.model_forms import RegistrationForm
+
+User = get_user_model()
 
 
 class LoginView(AuthLoginView):
@@ -23,5 +28,36 @@ class RegistrationView(FormView):
     success_url = reverse_lazy('main')
 
     def form_valid(self, form):
-        login(self.request, form.save())
+        form.save()
         return super().form_valid(form)
+
+
+class RegistrationConfirmView(RedirectView):
+    url = reverse_lazy('login')
+
+    def get(self, request, *args, **kwargs):
+        user = self.get_user(kwargs['uidb64'])
+
+        if user is not None:
+            token = kwargs['token']
+            if default_token_generator.check_token(user, token):
+                user.is_active = True
+                user.save(update_fields=('is_active',))
+                messages.success(
+                    request,
+                    'Activation success. '
+                    'You can login using your email and password.'
+                )
+            else:
+                messages.error(request, 'Activation error.')
+        return super().get(request, *args, **kwargs)
+
+    def get_user(self, uidb64):
+        try:
+            # urlsafe_base64_decode() decodes to bytestring
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = User.objects.get(id=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist,
+                ValidationError):
+            user = None
+        return user
